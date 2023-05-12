67a477a47424b3f3ed96ce4cd8bb59c59fca2688
xstapix <sarafannikov.85@mail.ru>
2023-05-12 19:02:26 +0300

    add express, 2 methods

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-  PORT: 8000,
-  hostName: 'localhost',
+  PORT: 3000,
+  hostName: '127.0.0.1',
   header: {
     ACA_Origin: "*",
     ACA_Credentials: "true",
-    ACA_Methods: "HEAD,OPTIONS,POST,PUT",
-    ACA_Headers: "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
-    Content_Type: "application/json"
+    ACA_Methods: "HEAD,OPTIONS,POST,PUT,GET",
+    ACA_Headers: "Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers",
+    Content_Type: "application/json,text/plain,multipart/form-data"
   }
 }
--- server.js
+++ server.js
@@ -1,33 +1,101 @@
-const http = require('http');
-const tgBot = require("./components/bot")
+const express = require('express')
+const axios = require('axios');
+const multer = require("multer");
+const url = require('url');
 
-const config = require('./config/serverConfig')
+const tgBot = require("./components/bot/index.js")
+const config = require('./config/serverConfig.js')
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", config.header.ACA_Origin);
-  res.setHeader("Access-Control-Allow-Credentials", config.header.ACA_Credentials);
-  res.setHeader("Access-Control-Allow-Methods", config.header.ACA_Methods);
-  res.setHeader("Access-Control-Allow-Headers", config.header.ACA_Methods);
-  res.setHeader("Content-Type", config.header.Content_Type)
+const app = express()
+const upload = multer({ dest: "uploads/" });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", config.header.ACA_Origin);
+  res.header("Access-Control-Allow-Credentials", config.header.ACA_Credentials);
+  res.header("Access-Control-Allow-Methods", config.header.ACA_Methods);
+  res.header("Access-Control-Allow-Headers", config.header.ACA_Headers);
+  res.header("Content-Type", config.header.Content_Type)
+  next();
+});
+
+app.post('/', (req, res) => {
   let data;
+  let clearData
   
   req.on("data", chunk => {
     data += chunk;
   });
 
-  req.on("end", () => {
+  req.on("end", async () => {
     if (data) { 
-      let clearData = JSON.parse(data.replace('undefined', ''))
-      console.log(clearData);
+      clearData = JSON.parse(data.replace('undefined', ''))
+      
+      if (clearData.method === 'sendTextMessageBot') {
+        console.log('sendTextMessageBot');
+        console.log(clearData);
+        
+        const chat_id_list = await axios.get('https://6392fd90ab513e12c5ff47f0.mockapi.io/location')
+        console.log(chat_id_list.data);
+
+        if (clearData.option === 'Всем') {
+          chat_id_list.data.map(async(objChat) => {
+            await axios.post(`https://api.telegram.org/bot6010251648:AAGMmNN6WO3uswC9nUyNIXUX6nmXjjytZEw/sendMessage`, {
+              chat_id: objChat.chat_id,
+              text: clearData.text
+            })
+          })
+        } else {
+          chat_id_list.data.map(async(objChat) => {
+            if (!objChat.active) {
+              await axios.post(`https://api.telegram.org/bot6010251648:AAGMmNN6WO3uswC9nUyNIXUX6nmXjjytZEw/sendMessage`, {
+                chat_id: objChat.chat_id,
+                text: clearData.text
+              })
+            }
+          })
+        }
+      }
+
+      if (clearData.method === 'dateRangeMessage') {
+        console.log(clearData);
+      }     
     }
-  });
-  if (req.url === '/api/id') {
-    res.end(JSON.stringify({status: 200}))
+  })
+})
+
+app.post('/file', upload.array("file"), async (req, res) => {
+  let filedata = req.files;
+  let bodydata = req.body;
+
+  if (bodydata.method === 'sendTextAndImgMessageBot') {
+    console.log(bodydata.method);
+    console.log(filedata);
+    // const chat_id_list = await axios.get('https://6392fd90ab513e12c5ff47f0.mockapi.io/location')
+    
+    let x = URL.createObjectURL(filedata[0])
+
+    console.log(x);
+    // const paramsObj = {
+    //   chat_id: 1226355897,
+    //   media: [
+    //     {
+    //       type: "photo",
+    //       media: url.fileURLToPath(filedata[0]),
+    //       caption: "clearData.text"
+    //     },
+    //     {
+    //       type: "photo",
+    //       media: 'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg'
+    //     }
+    //   ]
+    // }
+
+    // await axios.post(`https://api.telegram.org/bot6010251648:AAGMmNN6WO3uswC9nUyNIXUX6nmXjjytZEw/sendMediaGroup`, paramsObj) 
   }
+})
 
-});
-
-server.listen(config.PORT, config.hostName);
+app.listen(config.PORT, () => {
+  console.log(`Example app listening on port ${config.PORT}`)
+})
 
 tgBot.initBot()
